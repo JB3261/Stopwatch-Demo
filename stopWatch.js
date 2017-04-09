88ff5d36565ffde729761d9064319c284b483994
JB3261 <king.jubileu@gmail.com>
2017-04-09 20:53:50 +0200

    Added first iteration of code.

--- stopWatch.js
+++ stopWatch.js
@@ -1,7 +1,36 @@
 //global variables go here:
-
+var timer = 0;
+var interval;
+var running = false;
 
 //execute functions here:
-
+init();
 
 //function definitions go here:
+function init() {
+	document.getElementById("start").addEventListener("click", function(){
+		//Code to start/stop the stopwatch
+		if(!running){
+			interval = setInterval(function(){
+				timer++;
+				document.getElementById("timer").innerHTML = timer;
+				running = true;
+			}, 100);
+		} else {
+			clearInterval(interval);
+			running = false;
+		}		
+	});
+	
+	document.getElementById("reset").addEventListener("click", function(){
+		//Code to revert to initial state
+		
+	});
+	
+	document.getElementById("record").addEventListener("click", function(){
+		//Code to log current time to history
+	});
+	
+	
+}
+
